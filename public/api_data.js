c4b7a08ba4012ccc276ea8fe9b3cfa5a2ca98960
mharley12345 <info@mike-harley.tech>
2020-05-13 12:07:28 -0700

    updates the docs

--- public/api_data.js
+++ public/api_data.js
@@ -1,5 +1,87 @@
 define({ "api": [
   {
+    "type": "get",
+    "url": "/api/companies",
+    "title": "Get a list of all companies",
+    "name": "Companies",
+    "group": "Admin",
+    "permission": [
+      {
+        "name": "Admin"
+      }
+    ],
+    "success": {
+      "fields": {
+        "Success 200": [
+          {
+            "group": "Success 200",
+            "type": "String",
+            "optional": false,
+            "field": "Company_List",
+            "description": "<p>an array of companies</p>"
+          }
+        ]
+      }
+    },
+    "version": "0.0.0",
+    "filename": "routes/companies/company-router.js",
+    "groupTitle": "Admin"
+  },
+  {
+    "type": "get",
+    "url": "/api/companies/:id",
+    "title": "Get a compnay by Id",
+    "permission": [
+      {
+        "name": "Admin"
+      }
+    ],
+    "name": "Companies",
+    "group": "Admin",
+    "parameter": {
+      "fields": {
+        "Parameter": [
+          {
+            "group": "Parameter",
+            "type": "Number",
+            "optional": false,
+            "field": "id",
+            "description": "<p>Company id</p>"
+          }
+        ]
+      }
+    },
+    "header": {
+      "fields": {
+        "Header": [
+          {
+            "group": "Header",
+            "type": "String",
+            "optional": false,
+            "field": "Bearer_Token",
+            "description": "<p>auth token</p>"
+          }
+        ]
+      }
+    },
+    "success": {
+      "fields": {
+        "Success 200": [
+          {
+            "group": "Success 200",
+            "type": "String",
+            "optional": false,
+            "field": "Company",
+            "description": "<p>Returns the company matching the id</p>"
+          }
+        ]
+      }
+    },
+    "version": "0.0.0",
+    "filename": "routes/companies/company-router.js",
+    "groupTitle": "Admin"
+  },
+  {
     "type": "post",
     "url": "/api/auth/login",
     "title": "Admin login",
--- public/api_project.js
+++ public/api_project.js
@@ -1,14 +1,14 @@
 define({
   "name": "Enterprise Devices Api Docs",
   "version": "1.0.0",
-  "description": "api docs for enterprise devices app",
+  "description": "Api Docs For THe Enterprise Devices App",
   "title": "Enterprise Devices Api",
   "sampleUrl": false,
   "defaultVersion": "0.0.0",
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2020-05-13T18:19:35.655Z",
+    "time": "2020-05-13T19:06:36.145Z",
     "url": "http://apidocjs.com",
     "version": "0.22.1"
   }
