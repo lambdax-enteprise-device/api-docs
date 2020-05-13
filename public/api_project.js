fe200bbfd50fe68792a76cc1bc13a5941a832917
mharley12345 <info@mike-harley.tech>
2020-05-13 11:22:10 -0700

    init commit adds express server

---
+++ public/api_project.js
@@ -0,0 +1,15 @@
+define({
+  "name": "Enterprise Devices Api Docs",
+  "version": "1.0.0",
+  "description": "api docs for enterprise devices app",
+  "title": "Enterprise Devices Api",
+  "sampleUrl": false,
+  "defaultVersion": "0.0.0",
+  "apidoc": "0.3.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2020-05-13T18:19:35.655Z",
+    "url": "http://apidocjs.com",
+    "version": "0.22.1"
+  }
+});
