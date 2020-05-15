675a34556baf5a8ce55c3c4e84596f97e20adae9
mharley12345 <info@mike-harley.tech>
2020-05-15 11:32:09 -0700

    .

--- public/api_project.js
+++ public/api_project.js
@@ -1,14 +1,14 @@
 define({
   "name": "Enterprise Devices Api Docs",
   "version": "1.0.0",
-  "description": "Api Docs For THe Enterprise Devices App",
+  "description": "Api Docs For The Enterprise Devices App",
   "title": "Enterprise Devices Api",
   "sampleUrl": false,
   "defaultVersion": "0.0.0",
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2020-05-13T20:27:09.021Z",
+    "time": "2020-05-15T18:31:50.390Z",
     "url": "http://apidocjs.com",
     "version": "0.22.1"
   }
